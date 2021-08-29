30fb532221c9842681a9c2cce4da948c6cae5d35
last-las <478942543@qq.com>
2021-08-29 22:38:06 +0800

    add week1-hangman implement codes

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -1,40 +1,79 @@
-// Simple Hangman Program
-// User gets five incorrect guesses
-// Word chosen randomly from words.txt
-// Inspiration from: https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
-// This assignment will introduce you to some fundamental syntax in Rust:
-// - variable declaration
-// - string manipulation
-// - conditional statements
-// - loops
-// - vectors
-// - files
-// - user input
-// We've tried to limit/hide Rust's quirks since we'll discuss those details
-// more in depth in the coming lectures.
-extern crate rand;
-use rand::Rng;
-use std::fs;
 use std::io;
 use std::io::Write;
 
-const NUM_INCORRECT_GUESSES: u32 = 5;
-const WORDS_PATH: &str = "words.txt";
+fn get_a_word() -> String {
+    let str: String = String::from("mamemamehong");
+    return str;
+}
 
-fn pick_a_random_word() -> String {
-    let file_string = fs::read_to_string(WORDS_PATH).expect("Unable to read file.");
-    let words: Vec<&str> = file_string.split('\n').collect();
-    String::from(words[rand::thread_rng().gen_range(0, words.len())].trim())
+fn init_a_guess_word(n : usize) -> String {
+    let mut str : String = String::new();
+    for _i in 0..n {
+        str.push('-');
+    }
+    return str;
+}
+
+fn replace_i_word(word: String, i : usize, chr : char) -> String {
+    let mut x : usize = 0;
+    let mut new_word : String = String::new();
+    for c in word.chars() {
+        if x == i {
+            new_word.push(chr);
+        }else{
+            new_word.push(c);
+        }
+        x += 1;
+    }
+
+    return new_word;
 }
 
 fn main() {
-    let secret_word = pick_a_random_word();
-    // Note: given what you know about Rust so far, it's easier to pull characters out of a
-    // vector than it is to pull them out of a string. You can get the ith character of
-    // secret_word by doing secret_word_chars[i].
-    let secret_word_chars: Vec<char> = secret_word.chars().collect();
-    // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+    let w: String = get_a_word();
+    let mut guess_word : String = init_a_guess_word(w.len());
+    let mut cnt : i32 = 5;
+    let mut guessed_letter : String = String::new();
+    let n : usize = w.len();
+    while cnt != 0 {
+        println!("The word so far is {}", guess_word);
+        println!("You have guessed the following letters: {}", guessed_letter);
+        println!("You have {} guesses left", cnt);
 
-    // Your code here! :)
+        print!("Please guess a letter: ");
+
+
+        io::stdout()
+            .flush()
+            .expect("Error flushing stdout.");
+        let mut guess : String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+
+        guess = guess.replace("\n", "");
+        guessed_letter = guessed_letter + &guess;
+
+        let mut flag : bool = false;
+        for i in 0..n {
+            if w.chars().nth(i) == guess.chars().nth(0) {
+                flag = true;
+                guess_word = replace_i_word(guess_word, i, w.chars().nth(i).unwrap());
+            }
+        }
+
+        println!("");
+
+        if !flag {
+            cnt -= 1;
+        }else if guess_word == w {
+            print!("Success! The word is {}", guess_word);
+            break;
+        }
+
+        if cnt == 0 {
+            print!("failed! game over.");
+            break;
+        }
+    }
 }
